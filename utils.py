e8260d440c666ddd23f47c81a21d973fd8b784c3
AlexKharinKOLLC <alex@konstankino.com>
2018-12-13 11:05:32 +0200

    Add logger utils and exceptions handler for fetcher

--- utils.py
+++ utils.py
@@ -2,8 +2,30 @@ import os
 import errno
 import settings
 import tempfile
+import logging.handlers
 
 FIFO = os.path.join(tempfile.gettempdir(), settings.FIFO_NAME)
+
+
+def logger_init(func):
+    def inner(data):
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        handler = logging.handlers.SysLogHandler(address='/dev/log')
+        logger.addHandler(handler)
+        
+        return func(data, logger)
+    return inner
+
+    
+@logger_init
+def warning(data, logger):
+    logger.debug("WARNING: %s" % data)
+
+
+@logger_init
+def error(data, logger):
+    logger.debug("ERROR: %s" % data)
 
 
 def load_to_fifo(data):
